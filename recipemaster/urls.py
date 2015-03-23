88bef75ce223afc7f66d53cbc00c89d81a548184
Agnethe Søraa <me@agnethesoraa.com>
2015-03-23 21:31:47 +0100

    add redirect to index

--- recipemaster/urls.py
+++ recipemaster/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'recipemaster.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('recipes:index'))),
 
     url(r'^recipes/', include('recipemaster.recipes.urls', namespace='recipes')),
     url(r'^admin/', include(admin.site.urls)),
