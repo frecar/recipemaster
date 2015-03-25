5ad65cd503c0612c65757f6f8d11185c0b5763bc
Agnethe Søraa <me@agnethesoraa.com>
2015-03-25 21:26:42 +0100

    removed detail view

--- recipemaster/recipes/urls.py
+++ recipemaster/recipes/urls.py
@@ -5,5 +5,4 @@ from recipemaster.recipes import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<recipe_id>\d+)/$', views.recipe_detail, name='recipe_details'),
 )
--- recipemaster/recipes/views.py
+++ recipemaster/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from recipemaster.recipes.models import Recipe
 # Create your views here.
 
@@ -7,8 +7,3 @@ def index(request):
     latest_recipe_list = Recipe.objects.order_by('-id')
     context = {'latest_recipe_list': latest_recipe_list}
     return render(request, 'recipes/index.html', context)
-
-
-def recipe_detail(request, recipe_id):
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, 'recipes/details.html', {'recipe': recipe})
