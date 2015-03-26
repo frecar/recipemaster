d165e56a8c244393be96fcfbf2cf5ec6bbad9708
Agnethe Søraa <me@agnethesoraa.com>
2015-03-26 22:13:22 +0100

    add filter by tags

--- recipemaster/recipes/urls.py
+++ recipemaster/recipes/urls.py
@@ -5,4 +5,5 @@ from recipemaster.recipes import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
+    url(r'^by-tag/(?P<tag_id>\d+)/$', views.tag_filter, name='tag_filter')
 )
--- recipemaster/recipes/views.py
+++ recipemaster/recipes/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from recipemaster.recipes.models import Recipe
+from django.shortcuts import render,get_object_or_404
+from recipemaster.recipes.models import Recipe, Tag
 # Create your views here.
 
 
 def index(request):
-    latest_recipe_list = Recipe.objects.order_by('-id')
-    context = {'latest_recipe_list': latest_recipe_list}
+    recipes = Recipe.objects.order_by('-id')
+    context = {'recipes': recipes}
+    return render(request, 'recipes/index.html', context)
+
+
+def tag_filter(request, tag_id):
+    tag = get_object_or_404(Tag, pk=tag_id)
+    recipes = Recipe.objects.filter(tags=tag).order_by('id')
+    context = {'recipes': recipes}
     return render(request, 'recipes/index.html', context)
