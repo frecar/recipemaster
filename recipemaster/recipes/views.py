09c7388ca00a1cdc246f5336affe119ff44191a4
Agnethe Søraa <me@agnethesoraa.com>
2015-03-26 22:20:18 +0100

    fixed whitespace after comma

--- recipemaster/recipes/views.py
+++ recipemaster/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from recipemaster.recipes.models import Recipe, Tag
 # Create your views here.
 
