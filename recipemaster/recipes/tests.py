6677dbbe55ff04a2130f2177a3770fb5ddfa2624
Agnethe Søraa <me@agnethesoraa.com>
2015-03-27 16:43:31 +0100

    add test tag_filter view

--- recipemaster/recipes/tests.py
+++ recipemaster/recipes/tests.py
@@ -1,8 +1,14 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from recipemaster.recipes.models import Tag
 
 
 class SmokeTestCase(TestCase):
     def test_index(self):
         response = self.client.get(reverse('recipes:index'))
+        self.assertContains(response, 'Recipemaster')
+
+    def test_tag_filter(self):
+        tag = Tag.objects.create(title='Cake')
+        response = self.client.get(reverse('recipes:tag_filter', args=[tag.slug]))
         self.assertContains(response, 'Recipemaster')
