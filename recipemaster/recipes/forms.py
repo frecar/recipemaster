924c5dcae1285eb57138bf5880d1c833f68e656d
Agnethe Søraa <me@agnethesoraa.com>
2015-03-28 12:20:39 +0100

    add tags with recipesadd recipies view

--- recipemaster/recipes/forms.py
+++ recipemaster/recipes/forms.py
@@ -5,4 +5,8 @@ from recipemaster.recipes.models import Recipe
 class RecipeForm(ModelForm):
     class Meta:
         model = Recipe
-        fields = ['title', 'url']
+        fields = ['title', 'url', 'tags']
+
+    def __init__(self):
+        super().__init__()
+        self.fields['tags'].help_text = None
